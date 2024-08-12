d8a9731c08c4b03f6b2378e49e5ff698f4c90406
Siam <siambci@gmail.com>
2024-08-12 11:57:45 +0600

    user pey rent screen ongoing

--- lib/view/screen/user/PayRent/pay_rent_screen.dart
+++ lib/view/screen/user/PayRent/pay_rent_screen.dart
@@ -2,8 +2,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rentx/extension/my_extension.dart';
+import 'package:rentx/utils/app_string.dart';
 import 'package:rentx/view/component/text/common_text.dart';
 
+import '../../../../core/app_routes.dart';
 import '../../../../utils/app_colors.dart';
 
 class PayRentScreen extends StatelessWidget {
@@ -38,6 +40,38 @@ class PayRentScreen extends StatelessWidget {
                 ],
               )
             ),
+            12.height,
+            customContainer(
+              items: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CommonText(text: AppString.currentRentDue, fontSize: 18),
+                CommonText(text: "৳16,000", fontSize: 26, fontWeight: FontWeight.w700,)
+              ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () => Get.toNamed(AppRoutes.buyPackageScreen),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 30, vertical: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: AppColors.s500,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CommonText(
+                      text: "Buy Now",
+                      color: AppColors.black,
+                    ),
+                    8.width,
+                    Icon(Icons.arrow_forward)
+                  ],
+                ),
+              ),
+            )
 
           ],
         ),
